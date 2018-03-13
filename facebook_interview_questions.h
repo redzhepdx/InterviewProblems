6688e2ec897f2be7cdc3f339547f32de08c2213f
redzhepdx <redzhepdx@users.noreply.github.com>
2018-03-13 11:04:12 +0300

    Max Triplet Product

--- facebook_interview_questions.h
+++ facebook_interview_questions.h
@@ -36,3 +36,39 @@ int count_ways(std::vector<std::vector<int>> matrix, int row, int col) {
 }
 
 /**************************************COUNT_ALL_PATHS_TOP_TO_BOTTOM*********************/
+
+/***************************************MAX_TRIPLET_PRODUCT******************************/
+int max_product(std::vector<int> list){
+	int max_level_1 = INT_MIN;
+	int max_level_2 = INT_MIN;
+	int max_level_3 = INT_MIN;
+	int min_level_1 = INT_MAX;
+	int min_level_2 = INT_MAX;
+	
+	for(unsigned int i = 0; i < list.size(); i++){	
+		//Max 3 Elements in List
+		if(list[i] >= max_level_1){
+			max_level_3 = max_level_2;
+			max_level_2 = max_level_1;
+			max_level_1 = list[i];
+		}
+		else if(list[i] >= max_level_2){
+			max_level_3 = max_level_2;
+			max_level_2 = list[i];
+		}
+		else if(list[i] >= max_level_3){
+			max_level_3 = list[i];
+		}
+
+		if(list[i] <= min_level_1){
+			min_level_2 = min_level_1;
+			min_level_1 = list[i];
+		}else if(list[i] <= min_level_2){
+			min_level_2 = list[i];
+		}
+	}
+	std::cout << "Max 1 : " << max_level_1 << " Max 2 : " << max_level_2 << " Max 3 : " << max_level_3 << " Min 1 : " << min_level_1 << " Min 2 : " << min_level_2 << std::endl;
+	return std::max(max_level_3 * max_level_2 * max_level_1, max_level_1 * min_level_1 * min_level_2);
+
+}
+/***************************************MAX_TRIPLET_PRODUCT******************************/
